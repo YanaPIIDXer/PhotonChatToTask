d91c1d7c620f65bd7c6324731371b7d12bbbaa63
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-31 07:31:17 +0900

    IChatClientListenerを実装

--- Assets/Plugins/Runtime/Scripts/PhotonChatToTask/Callbacks.cs
+++ Assets/Plugins/Runtime/Scripts/PhotonChatToTask/Callbacks.cs
@@ -1,13 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Chat;
+using ExitGames.Client.Photon;
 
 namespace PhotonChatToTask
 {
     /// <summary>
     /// コールバック
     /// </summary>
-    public class Callbacks : MonoBehaviour
+    public class Callbacks : MonoBehaviour, IChatClientListener
     {
+        public void DebugReturn(DebugLevel level, string message)
+        {
+        }
+
+        public void OnChatStateChange(ChatState state)
+        {
+        }
+
+        public void OnConnected()
+        {
+        }
+
+        public void OnDisconnected()
+        {
+        }
+
+        public void OnGetMessages(string channelName, string[] senders, object[] messages)
+        {
+        }
+
+        public void OnPrivateMessage(string sender, object message, string channelName)
+        {
+        }
+
+        public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+        {
+        }
+
+        public void OnSubscribed(string[] channels, bool[] results)
+        {
+        }
+
+        public void OnUnsubscribed(string[] channels)
+        {
+        }
+
+        public void OnUserSubscribed(string channel, string user)
+        {
+        }
+
+        public void OnUserUnsubscribed(string channel, string user)
+        {
+        }
     }
 }
